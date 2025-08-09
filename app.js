c78c393ac61e853c03d25aeac19196e82ddc0598
Douglas Alves <douglasbrasil815@gmal.com>
2025-08-09 14:49:29 -0300

    Sistema JS para adicionar os nomes dos amigos - finalizado

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+function adicionarAmigo(){
+     amigo_digitado = document.getElementById("amigo").value
+
+     // Verifica se o campo de nome foi preenchido ou se contém apenas numeros
+     if (amigo_digitado == [] || !isNaN(amigo_digitado)) {
+          alert("Insira um nome válido")
+     } else {
+          // Cria o elemento <li> e adiciona ele na estrutura do html da página
+          amigos.push(amigo_digitado)
+          let linha = document.createElement("li")
+          linha.innerHTML = amigo_digitado
+          document.getElementById("listaAmigos").append(linha)
+
+          //Limpa o campo de nome caso o nome for válido
+          document.getElementById("amigo").value = null
+     }
+
+}
