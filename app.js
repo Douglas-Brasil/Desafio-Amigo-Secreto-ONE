6d1a8730e1903c279bb0598eb51ecfc23e4f505e
Douglas Alves <douglasbrasil815@gmal.com>
2025-08-19 15:30:24 -0300

    Sortear amigo, exibir o resultado e remover lista de amigos do HTML

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-let amigos = []
+let amigos = [];
+
+function inserirItemHTML(itemInserirNoHTML) {
+     // Cria o elemento <li> e adiciona ele na estrutura do html da página
+     let linha = document.createElement("li");
+     linha.innerHTML = itemInserirNoHTML;
+     document.getElementById("listaAmigos").append(linha);
+
+     //Limpa o campo de nome caso o nome for válido
+     document.getElementById("amigo").value = null;
+
+};
+
+function removerListaAmigosHTML() {
+     lista = document.getElementById("listaAmigos").innerHTML = '';
+};
+
+function aparecerResultadoSorteio (resultado) {
+     document.getElementById("resultado").innerHTML = "O Resultado do Sorteio é: " + resultado;
+}
+
 function adicionarAmigo(){
-     amigo_digitado = document.getElementById("amigo").value
-
+     amigoDigitado = document.getElementById("amigo").value;
      // Verifica se o campo de nome foi preenchido ou se contém apenas numeros
-     if (amigo_digitado == [] || !isNaN(amigo_digitado)) {
-          alert("Insira um nome válido")
+     if (amigoDigitado == [] || !isNaN(amigoDigitado)) {
+          alert("Insira um nome válido");
      } else {
-          // Cria o elemento <li> e adiciona ele na estrutura do html da página
-          amigos.push(amigo_digitado)
-          let linha = document.createElement("li")
-          linha.innerHTML = amigo_digitado
-          document.getElementById("listaAmigos").append(linha)
-
-          //Limpa o campo de nome caso o nome for válido
-          document.getElementById("amigo").value = null
+          amigos.push(amigoDigitado);
+          inserirItemHTML(amigoDigitado);
      }
+
+};
+
+function sortearAmigo() {
+     //Pega a quantidade de amigos 
+     quantidadeAmigos = amigos.length;
+     // Math.random() gera um número entre 0 e 0.9999, então multiplicamos o numero gerado com a qauntidade de amigos
+     // Math.floor() desconsidera o valor decimal do numero gerado, deixa apenas o valor inteiro
+     amigoSorteado = Math.floor(Math.random() * quantidadeAmigos);
+
+     removerListaAmigosHTML();
+     aparecerResultadoSorteio(amigos[amigoSorteado]);
 
 }
